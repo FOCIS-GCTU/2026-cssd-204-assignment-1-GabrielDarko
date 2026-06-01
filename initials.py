dd883fcd08b60c0e66dc9dbb9c868066581055ce
DARKO GABRIEL OFOSU <2425402564@live.gctu.edu.gh>
2026-06-01 17:58:13 +0000

    Assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
+# File:initials.py
+#Description: Print my initials (GOD) in stylized large block letters.
+#Assignment Number: 1
+#
+#Name: Gabriel Ofosu Darko
+#STUDENT ID:2425402564
+#Email:2425402564@live.gctu.edu.gh
+#Grader:Augustus Buckman
+#
+#On my honor, Gabriel Ofosu Darko, this programing assignment ism own work
+#and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Use a breakpoint in the code line below to debug your script.
+    print( )
+    print("...GOD")
+    print( )
+    print("......GGGGGG...............OOOO.............DDDDDDDD.........")
+    print("....GG......GG...........OO....OO...........DD......DD.......")
+    print("...GG........GG.........OO......OO..........DD.......DD......")
+    print("...GG..................OO........OO.........DD........DD.....")
+    print("...GG....GGGGGG........OO........OO.........DD........DD.....")
+    print("...GG........GG........OO........OO.........DD........DD.....")
+    print("...GG........GG........OO........OO.........DD........DD.....")
+    print("...GG........GG.........OO......OO..........DD........DD.....")
+    print("....GG......GG....**.....OO....OO.....**... DD......DD.....**")
+    print("......GGGGGG......**.......OOOO.......**....DDDDDDDD.......**")
+
+    # Press Ctrl+F8 to toggle the breakpoint.
 
 
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
 
-main()
